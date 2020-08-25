4bf9c94aab5bb5df437170a91ea5268ce212289a
Tanvir Hossen <r3dr0k37@gmail.com>
2020-08-25 20:21:38 +0600

    Mail Setting

--- resources/views/layouts/app_header.blade.php
+++ resources/views/layouts/app_header.blade.php
@@ -28,9 +28,7 @@
               <a href="/profile" class="dropdown-item has-icon">
                 <i class="far fa-user"></i> Profile
               </a>
-              <a href="features-activities.html" class="dropdown-item has-icon">
-                <i class="fas fa-bolt"></i> Activities
-              </a>
+              
               <a href="/settings" class="dropdown-item has-icon">
                 <i class="fas fa-cog"></i> Settings
               </a>
